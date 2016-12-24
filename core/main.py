54a2f8ad604b40501045e5bef8cd44a82300d82e
Akshay H <thesmileynoob@gmail.com>
2016-12-25 00:14:55 +0530

    Movement ctrl fixed. +render method
    Added setup_window() and render()

--- core/main.py
+++ core/main.py
@@ -8,30 +8,73 @@ from .CONFIG import CONFIG
 
 from .components.ships.ship import Ship
 
+
+def setup_window():
+    pygame.init()
+    pygame.display.set_caption(CONFIG.window_title)
+
+
+
+
 def start_game():
     """
     Initialize pygame, check saved files, and run the main loop
     """
-    pygame.init()
+    clock = pygame.time.Clock()
+    setup_window()
     screen = pygame.display.set_mode(CONFIG.window_size)
-    ship = Ship("resources/ships/playership.png", (50,0,0), "hero")
+    ship = Ship("resources/ships/playership.png", (140,400,0), "hero")
 
 
-    while 1:
+    def reset():
+        return Ship("resources/ships/playership.png", (140, 400, 0), "hero")
+    quit = False
+    while not quit:
         for event in pygame.event.get():
-            print event
-            if event.type == pygame.QUIT: sys.exit()
+            # print(event)
+            if event.type == pygame.QUIT: quit = True
+            if event.type == 3:
+                if event.key == 27: quit = True  # "Esc" -> quit
+                if event.key == 114: ship = reset() # "R" -> reset level
+        # Ship movement
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            if keys[pygame.K_UP]:
+                ship.move(-5, -5)
+            elif keys[pygame.K_DOWN]:
+                ship.move(-5, 5)
+            else:
+                ship.move(-5, 0)
 
-        #TODO Make an independent render method
+        elif keys[pygame.K_RIGHT]:
+            if keys[pygame.K_UP]:
+                ship.move(5, -5)
+            elif keys[pygame.K_DOWN]:
+                ship.move(5, 5)
+            else:
+                ship.move(5, 0)
+        elif keys[pygame.K_UP]:
+            if keys[pygame.K_LEFT]:
+                ship.move(-5, -5)
+            elif keys[pygame.K_RIGHT]:
+                ship.move(5, -5)
+            else:
+                ship.move(0, -5)
+        elif keys[pygame.K_DOWN]:
+            if keys[pygame.K_LEFT]:
+                ship.move(-5, 5)
+            elif keys[pygame.K_RIGHT]:
+                ship.move(5, 5)
+            else:
+                ship.move(0, 5)
+        if keys[pygame.K_SPACE]:
+            print("Pew pew pew..")
 
-        screen.blit(ship.pimg(), ship.get_position())
-        ship.move(0.1,0)
-        pygame.display.flip()
+        ship.move(0,1)
         screen.fill((0,0,0))
-
-        ship.move(0, 0.12)
         screen.blit(ship.pimg(), ship.get_position())
         pygame.display.flip()
-        time.sleep(0.012)
 
+        pygame.display.flip()
+        clock.tick(60)
     pass
