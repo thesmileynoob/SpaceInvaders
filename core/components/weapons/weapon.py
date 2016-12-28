2aab937b50d44089e8067da3851b31b0ba5a4960
Akshay H <thesmileynoob@gmail.com>
2016-12-28 23:00:08 +0530

    Added a base Weapon class

---
+++ core/components/weapons/weapon.py
@@ -0,0 +1,32 @@
+"""
+The Weapon base class
+"""
+import pygame
+class Weapon(object):
+    """
+    Weapon base class
+    A weapon is a self contained entity consisting of
+        - Damage
+        - Ammo
+    """
+    _IMAGE="res/weapons/laser_red.png"
+    def __init__(self, image=_IMAGE, ammo_count=100, sound=None):
+        """ ${:TODO Docs} """
+        self.image = pygame.image.load(image).convert_alpha() 
+        self.ammo_count = ammo_count
+        self.sound = sound 
+
+    def fire(self):
+        """ Fire the weapon """
+        if self.ammo_count <= 0:
+            self.ammo_count = 0
+            return
+        else:
+            self.ammo_count -= 1 
+            print(self.sound)
+            return 
+
+    def render(self):
+        """ Render the weapon """
+        pass
+
