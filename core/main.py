106b3f37749ed95e399da4059752eeb2e66d7565
Akshay H <thesmileynoob@gmail.com>
2016-12-29 00:00:41 +0530

    Major update
    - Moved input response code to player ship
    - Added fps counter

--- core/main.py
+++ core/main.py
@@ -9,6 +9,8 @@ import pygame
 from .CONFIG import CONFIG
 from .components.levels.level0 import Level0
 from .components.ships.player import Player
+from .event import EventMaker
+from .messagequeue import MessageQueue
 
 
 
@@ -29,64 +31,54 @@ def start_game():
     #########  GLOBAL SETUP
     clock = pygame.time.Clock()
     setup_window()
+    event_maker = EventMaker()
+    Q = MessageQueue()
+    EM = EventMaker()
     #########
 
     ####### LEVEL Setup
     # ship = Player()
-    level = Level0()
+    level = Level0(Q)
     ship = level.player
     #######
 
+    ### Testing
+    font = pygame.font.SysFont('ubuntu', 25)
+    ###
+    counter = 0
+    count = 0
+    time1 = time.time()
     def reset():
         return Ship("resources/ships/playership.png", (140, 400, 0), "hero")
     quit = False
     while not quit:
-        for event in pygame.event.get():
+        counter += 1
+
+        for event in pygame.event.get(): # Quit clause
             # print(event)
             if event.type == pygame.QUIT: quit = True
             if event.type == 3:
                 if event.key == 27: quit = True  # "Esc" -> quit
                 if event.key == 114: ship = reset() # "R" -> reset level
-        # Ship movement
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            if keys[pygame.K_UP]:
-                ship.move_by(-3, -5)
-            elif keys[pygame.K_DOWN]:
-                ship.move_by(-3, 5)
-            else:
-                ship.move_by(-5, 0)
+        # Messaging
+        Q.send_message(EM.tick_event(1, pygame.key.get_pressed())) 
+        Q.dispatch()
 
-        elif keys[pygame.K_RIGHT]:
-            if keys[pygame.K_UP]:
-                ship.move_by(3, -5)
-            elif keys[pygame.K_DOWN]:
-                ship.move_by(3, 5)
-            else:
-                ship.move_by(5, 0)
-        elif keys[pygame.K_UP]:
-            if keys[pygame.K_LEFT]:
-                ship.move_by(-3, -5)
-            elif keys[pygame.K_RIGHT]:
-                ship.move_by(3, -5)
-            else:
-                ship.move_by(0, -5)
-        elif keys[pygame.K_DOWN]:
-            if keys[pygame.K_LEFT]:
-                ship.move_by(-3, 5)
-            elif keys[pygame.K_RIGHT]:
-                ship.move_by(3, 5)
-            else:
-                ship.move_by(0, 5)
-        if keys[pygame.K_SPACE]:
-            ship.fire1()
-
-        ship.move_by(0,0.1)
         screen.fill((0,0,0))
         x, y = level.render()
         for yy in y:
             screen.blit(x, yy)
         screen.blit(*ship.render())
+
+        # FPS
+        if time.time() - time1 >= 1: 
+            # print(f"FPS: {counter}")
+            time1 = time.time()
+            count = counter
+            counter = 0 
+        screen.blit(font.render(f"{count}",True, (255,255,255)), (560,0))
+
+
         pygame.display.flip()
         clock.tick(60)
     pass
