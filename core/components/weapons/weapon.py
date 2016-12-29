626d586a9d40b96728499b6e1a4c2cfe680adccd
Akshay H <thesmileynoob@gmail.com>
2016-12-29 23:19:48 +0530

    Enables messaging
    Weapons now send a fire message to chaosmaker.
    The message payload must contain all the spec for chaosmaker to create
    a chaos object

--- core/components/weapons/weapon.py
+++ core/components/weapons/weapon.py
@@ -2,6 +2,8 @@
 The Weapon base class
 """
 import pygame
+from ...specials.messaging import Message
+
 class Weapon(object):
     """
     Weapon base class
@@ -10,20 +12,35 @@ class Weapon(object):
         - Ammo
     """
     _IMAGE="res/weapons/laser_red.png"
-    def __init__(self, image=_IMAGE, ammo_count=100, sound=None):
+    def __init__(self, Q, image=_IMAGE, ammo_count=1000, sound=None):
         """ ${:TODO Docs} """
         self.image = pygame.image.load(image).convert_alpha() 
         self.ammo_count = ammo_count
         self.sound = sound 
         self.position = 10,10
+        self.send_message = Q.send_message
 
-    def fire(self):
-        """ Fire the weapon """
+    def fire(self, position):
+        """ Fire the weapon 
+        from position 
+        """
         if self.ammo_count <= 0:
             self.ammo_count = 0
             return
         else:
             self.ammo_count -= 1 
+            message = Message("weapon", "chaosmaker", {
+                "name": "laser",
+                "image": "res/weapons/laser_red.png",
+                "position": position,
+                "velocity": (0, -2),
+                "drag": (0,0),
+                "rotation": 0,
+                "target": None,
+                "anim_start": None,
+                "anim_end": None,
+                })
+            self.send_message(message)
             print(self.sound)
             return 
 
